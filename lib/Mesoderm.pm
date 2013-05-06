dd9a03195c3c2b012a694760a978e7496e4f0456
Graham Barr <gbarr@pobox.com>
2013-05-06 10:05:02 -0500

    Add resultset_components

--- lib/Mesoderm.pm
+++ lib/Mesoderm.pm
@@ -87,6 +87,7 @@ sub table_components {
 }
 
 sub column_components { my ($self, $column) = @_; return; }    # TODO
+sub resultset_components { return; }
 
 sub table_roles {
   my ($self, $table) = @_;
@@ -520,6 +521,17 @@ sub write_table {
   print $fh "    extends 'DBIx::Class::ResultSet';\n";
   print $fh "    with qw(",join("\n      ","",$self->resultset_roles($table), $mesoderm_role ,");\n");
   print $fh "    no Moose;\n";
+
+  my @rs_comp =
+    sort { $a->order <=> $b->order or $a->name cmp $b->name }
+    map { Mesoderm::Component->find($_) }
+    $self->resultset_components($table);
+
+  if (@rs_comp) {
+    my $list = join " ", map { $_->name } @rs_comp;
+    print $fh "\n";
+    print $fh "  __PACKAGE__->load_components(qw/ $list /);\n";
+  }
   print $fh "  }\n";
   print $fh "  __PACKAGE__->resultset_class('$resultset_class');\n";
   print $fh "  $dbic_class->register_class( $moniker => __PACKAGE__ );\n";
@@ -779,6 +791,10 @@ Defaults to C<resultset_class_namespace> plus C<::Role>
 =head1 METHODS
 
 =over
+
+=item resultset_components ( $table )
+
+Returns a list of L<DBIx::Class> components to be loaded by the resultset class
 
 =item table_components ( $table )
 
